8ad1c6b34b6dbe9cf3c781a0160d41d0a7299b2d
kingssik <hyunsikkang25@gmail.com>
2023-08-16 13:25:21 +0900

    게시물 작성시 날짜 및 시간 저장

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -29,12 +29,13 @@ class Main {
         int id = lastArticleId + 1;
         lastArticleId = id;
 
+        String regDate = Util.getNowDateStr();
         System.out.printf("제목 : ");
         String title = sc.nextLine();
         System.out.printf("내용 : ");
         String body = sc.nextLine();
 
-        Article article = new Article(id, title, body);
+        Article article = new Article(id, regDate, title, body);
         articles.add(article);
 
         System.out.printf("%d번 글이 생성 되었습니다.\n", id);
@@ -81,7 +82,7 @@ class Main {
         }
 
         System.out.printf("번호 : %d\n", foundArticle.id);
-        System.out.printf("날짜 : 2023-12-09 12:12:12\n");
+        System.out.printf("날짜 : %s\n", foundArticle.regDate);
         System.out.printf("제목 : %s\n", foundArticle.title);
         System.out.printf("내용 : %s\n", foundArticle.body);
 
@@ -108,11 +109,13 @@ class Main {
 
 class Article {
   int id;
+  String regDate;
   String title;
   String body;
 
-  Article(int id, String title, String body) {
+  Article(int id, String regDate, String title, String body) {
     this.id = id;
+    this.regDate = regDate;
     this.title = title;
     this.body = body;
   }
